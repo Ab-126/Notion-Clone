784a73ed37a9bae22c26c7af8aa6d0ac75759dac
unknown <geekisaprogrammer@gmail.com>
2024-01-27 16:19:40 +0530

    first commit

---
+++ components/providers/convex-provider.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { ReactNode } from "react";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+
+// The ! (exclamation mark) denotes that the variable is not null and not undefined
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
---
+++ convex/auth.config.js
@@ -0,0 +1,10 @@
+export default {
+  providers: [
+    {
+      domain: "https://adapted-escargot-28.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+// npx convex dev to start the server
